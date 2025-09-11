716b613f8220ef83b6b9b463dfce5bf0e04ab3e1
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-11 23:19:47 +0530

    refactor: update system message for interview grading and enhance JSON output structure

--- invoke/app.js
+++ invoke/app.js
@@ -16,20 +16,39 @@ async function main() {
     // stop: "ga", // Negative
     // max_completion_tokens: 1000,
     // max_tokens: '',
+    response_format: {type: 'json_object'},
 
     model: 'llama-3.3-70b-versatile',
     messages: [
       {
         role: 'system',
-        // content:
-        //   'You are Jarvis, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. Output must be a single word',
+        content: `You are an interview grader assistant. Your task is to generate candidate evaluation
+          score. Output must be following JSON structure:
+          {
 
-        content: `You are Jarvis, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. You must return the result in valid JSON structure.
-          example : {"sentiment": "Negative"}`,
+          'confidence": number (1r10 scale),
+          "accuracy": number (1-10 scale),
+          "pass": boolean (true or false)
+          }
+
+          The response must:
+          1. Include ALL fields shown above
+
+          2. Use only the exact field names shown
+          3. Follow the exact data types specified
+          4. Contain ONLY the JSON object and nothing else `,
       },
       {
         role: 'user',
-        content: `Review: These headphones arrived quickly and look great, but the left earcup stopped working after a week. Sentiment:`,
+        content: ` Q: What does = do in JavaScript?
+                    A: It checks strict equality-both value and type must match.  
+                    Q: How do you create a promise that resolves after 1 second?
+                    A: const p = new Promise(r => setTimeout(r, 1000));
+                    Q: What is hoisting?
+                    A: JavaScript moves declarations (but.not initializations) to the top of their scope before code runs.
+                    Q: Why use let instead of var?
+                    A: tet is block-scoped, avoiding the function-scope quirks and re-declaration issues of var.,`
+   
       },
     ],
   });
