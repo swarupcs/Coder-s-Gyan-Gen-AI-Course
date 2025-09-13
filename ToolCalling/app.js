ab0741f48bfa361ec6cf5ab1513d7e9604a42f33
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-09-13 12:11:26 +0530

    refactor: integrate Tavily API for enhanced web search functionality

--- ToolCalling/app.js
+++ ToolCalling/app.js
@@ -1,7 +1,11 @@
 import Groq from 'groq-sdk';
-
+import { tavily } from '@tavily/core';
 import dotenv from 'dotenv';
 dotenv.config();
+
+const tvly = tavily({
+  apiKey: process.env.TAVILY_API_KEY,
+});
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
@@ -10,10 +14,7 @@ const groq = new Groq({
 // console.log('process.env.GROQ_API_KEY', process.env.GROQ_API_KEY);
 
 async function main() {
-  const completion = await groq.chat.completions.create({
-    model: 'llama-3.3-70b-versatile',
-    temperature: 0,
-    messages: [
+  const messages = [
       {
         role: 'system',
         content: `You are a smart personal assistant who answers the asked questions.
@@ -25,7 +26,11 @@ async function main() {
         role: 'user',
         content: 'When was iphone 16 launched?',
       },
-    ],
+    ];
+  const completion = await groq.chat.completions.create({
+    model: 'llama-3.3-70b-versatile',
+    temperature: 0,
+    messages: messages,
     tools: [
       {
         type: 'function',
@@ -49,41 +54,81 @@ async function main() {
     tool_choice: 'auto',
   });
 
+  messages.push(completion.choices[0].message);
+
   const toolCalls = completion.choices[0].message.tool_calls;
 
   // console.log("toolCalls", toolCalls);
 
-  if(!toolCalls) {
+  if (!toolCalls) {
     console.log(`Assistant: ${completion.choices[0].message.content}`);
     return;
   }
 
-
-  for(const tool of toolCalls) {
+  for (const tool of toolCalls) {
     console.log('Tool call:', tool);
     const functionName = tool.function.name;
     const functionParams = tool.function.arguments;
 
-    if(functionName === 'webSearch') {
-      const toolResult = await webSearch((JSON.parse(functionParams)));
+    if (functionName === 'webSearch') {
+      const toolResult = await webSearch(JSON.parse(functionParams));
       console.log('Tool result:', toolResult);
-  } 
 
+      messages.push({
+        tool_call_id: tool.id,
+        role: 'tool',
+        name: functionName,
+        content: toolResult,
+      });
+    }
 
-  // console.log(JSON.stringify(completion.choices[0].message.content));
-}
+      const completions2 = await groq.chat.completions.create({
+        model: 'llama-3.3-70b-versatile',
+        temperature: 0,
+        messages: messages,
+        tools: [
+          {
+            type: 'function',
+            function: {
+              name: 'webSearch',
+              description:
+                'Search the latest information and realtime data on the internet',
+              parameters: {
+                type: 'object',
+                properties: {
+                  query: {
+                    type: 'string',
+                    description: 'The search query to perform search on',
+                  },
+                },
+                required: ['query'],
+              },
+            },
+          },
+        ],
+        tool_choice: 'auto',
+      });
+
+      console.log(JSON.stringify(completions2.choices[0].message, null, 2 ));
+
+    // console.log(JSON.stringify(completion.choices[0].message.content));
+  }
 }
 
 main();
 
-async function webSearch({ query}) {
+async function webSearch({ query }) {
   // Here we will do tavily api call
 
-  console.log("tool call with query:", query);
+  console.log('tool call with query:', query);
 
-  return 'Iphone 16 was launched on September 12, 2024.';
+  const response = await tvly.search(query);
+
+  console.log(response);
+
+
+  const finalResponse = response.results.map((result) => result.content).join('\n\n');
+  console.log("response", finalResponse);
+  return finalResponse;
+  // return 'Iphone 16 was launched on September 12, 2024.';
 }
-
- 
-
-
